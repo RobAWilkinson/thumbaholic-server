383b53b2bb208a04b2e3bcad84f4def5bb51d52a
Rob Wilkinson <wilkinson.robert.a@gmail.com>
2016-06-11 23:52:56 -0700

    fix

--- index.js
+++ index.js
@@ -162,7 +162,6 @@ wss.on('connection', function connection (ws) {
   var location = url.parse(ws.upgradeReq.url, true)
   // you might use location.query.access_token to authenticate or share sessions
   // or ws.upgradeReq.headers.cookie (see http://stackoverflow.com/a/16395220/151312)
-  connections.push(ws)
   ws.on('message', function incoming (message) {
     console.log('received a websocket message')
     let data = JSON.parse(message)
